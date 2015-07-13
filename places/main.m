8296c4a4db4e598814a133625e4cde4ae8c608de
Zordius Chen <zordius@yahoo-inc.com>
2015-07-13 14:32:56 +0800

    first commit

---
+++ places/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  places
+//
+//  Created by Zordius Chen on 7/13/15.
+//  Copyright (c) 2015 Zordius Chen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
