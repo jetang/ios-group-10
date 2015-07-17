8f49162406742b1c02143c65bd4a78fc7867a94e
Jeff Tang <jetang@yahoo-inc.com>
2015-07-17 12:20:12 +0800

    WIP (not working)

--- places/placesMapViewController.m
+++ places/placesMapViewController.m
@@ -8,6 +8,7 @@
 
 #import "placesMapViewController.h"
 #import <CoreLocation/CoreLocation.h>
+#import "facebookPlaces.h"
 
 @implementation placesMapViewController
 
@@ -16,6 +17,29 @@
     CLLocationCoordinate2D coord = CLLocationCoordinate2DMake(25.04,121.55);
     MKCoordinateRegion region = MKCoordinateRegionMakeWithDistance(coord, 10000, 10000);
     [self.mapView setRegion:region animated:YES];
+    
+    [[facebookPlaces getInstance] addObserver:self
+                  forKeyPath:@"places"
+                     options:0
+                     context:nil];
+}
+
+- (void)observeValueForKeyPath:(NSString*)keyPath
+                      ofObject:(id)object
+                        change:(NSDictionary*)change
+                       context:(void*)context
+{
+    if ([keyPath isEqualToString:@"places"]) {
+        // [self.tableView reloadData];
+        
+        NSMutableDictionary *data = [facebookPlaces getInstance].places[0];
+        NSLog(@"name %@", data[@"name"]);
+    } else {
+        [super observeValueForKeyPath:keyPath
+                             ofObject:object
+                               change:change
+                              context:context];
+    }
 }
 
 @end
