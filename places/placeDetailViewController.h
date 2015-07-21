bc7d1c26280dc06388ad72018d11bc3ca37799b2
Augustin Wang <augustin@governhead-lm.tpcity.corp.yahoo.com>
2015-07-21 12:52:49 +0800

    add detail view

--- places/placeDetailViewController.h
+++ places/placeDetailViewController.h
@@ -10,4 +10,12 @@
 
 @interface placeDetailViewController : UIViewController
 
+@property (weak, nonatomic) IBOutlet UILabel *titleLabel;
+@property (weak, nonatomic) IBOutlet UILabel *streetLabel;
+@property (weak, nonatomic) IBOutlet UILabel *cityLabel;
+@property (weak, nonatomic) IBOutlet UILabel *countryLabel;
+@property (weak, nonatomic) IBOutlet UILabel *zipLabel;
+
+@property (strong, nonatomic) NSMutableDictionary *data;
+
 @end
